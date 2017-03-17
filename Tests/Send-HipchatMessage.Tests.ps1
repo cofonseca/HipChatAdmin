d72547f6db12513fb1a41b86a6bc970673f5219a
Corey Fonseca <cfonseca@blueport.com>
2017-03-17 15:53:47 -0400

    Added module and manifest files. Updated Pester test for Send-HipchatMessage.

---
+++ Tests/Send-HipchatMessage.Tests.ps1
@@ -0,0 +1,13 @@
+Import-Module PSHipChat
+
+Describe "Send-HipchatMessage" {
+
+    Context "When the API call is sent" {
+
+        It "Should return a 204 Status Code" {
+            Send-HipchatMessage 'Running Pester Unit Test...' -RoomName $RoomName -Notify $false | 
+            Should Be '204'
+        }
+    }
+
+}
