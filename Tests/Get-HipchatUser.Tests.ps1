b2532b6af9bc784549d1069872909fe1666f615a
Corey Fonseca <cofonseca@gmail.com>
2017-04-02 13:20:40 -0400

    Added test for Get-HipchatUser.

---
+++ Tests/Get-HipchatUser.Tests.ps1
@@ -0,0 +1,25 @@
+Import-Module PSHipChat
+
+Describe "Get-HipchatUser" {
+
+    Context "When the API call is sent" {
+
+        BeforeAll {
+
+            $Users = Get-HipchatUser -ApiToken $ApiToken
+        }
+
+        It "Should return PSObjects" {
+            $Users[0] | Should BeOfType PSCustomObject
+        }
+
+        It "Should contain a property with the user's name" {
+            $Users[0].Name | Should Not BeNullOrEmpty
+        }
+
+        It  "Should contain a property with the user's mention name" {
+            $Users[0].MentionName | Should Not BeNullOrEmpty
+        }
+
+    }
+}
