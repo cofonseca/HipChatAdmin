81b10fbe6618631c886ea12c99a4f8d219254a50
Corey Fonseca <cfonseca@blueport.com>
2017-05-04 08:37:06 -0400

    Added Remove-HipchatUserFromRoom

---
+++ HipChatAdmin/Public/Remove-HipchatUserFromRoom.ps1
@@ -0,0 +1,63 @@
+#requires -version 5
+
+<#
+.SYNOPSIS
+	Removes a HipChat user from a HipChat room.
+.DESCRIPTION
+	Removes an existing HipChat user from an existing HipChat room.
+.PARAMETER MentionName
+	Required: The user's mention name (@name).
+.PARAMETER RoomName
+	Required: The name of the room.
+.PARAMETER ApiToken
+	Required: Your HipChat API Token. You can obtain an API Token from the
+	HipChat website by navigating to Account Settings > API Access.
+.NOTES
+	Version:        1.0
+	Author:         Corey Fonseca
+	Creation Date:  5/4/17
+	Purpose/Change: Full Functionality
+.EXAMPLE
+	Remove-HipchatUserFromRoom -MentionName 'JohnSmith' -RoomName 'Development' -ApiToken 'REXsCauSe553gsoIJg1Gj4zwNsSAwS'
+	Removes @JohnSmith from the Development room.
+
+#>
+function Remove-HipchatUserFromRoom{
+
+    [CmdletBinding()]
+	Param(
+		[Parameter(Mandatory=$true,ValueFromPipeline=$true,HelpMessage="Enter the users mention name (@name)")][Alias('UserName')][string]$MentionName,
+    	[Parameter(Mandatory=$true,ValueFromPipeline=$true,HelpMessage="Enter the room name")][string]$RoomName,
+		[Parameter(Mandatory=$true,ValueFromPipeline=$true,HelpMessage="Enter your API Token")][Alias('ApiKey')][string]$ApiToken
+    )
+
+    BEGIN {
+
+		$Uri = "https://api.hipchat.com/v2/room/"+$RoomName+"/member/@"+$MentionName+"?auth_token="+$ApiToken
+    }
+
+    PROCESS {
+
+		# Send API Request #
+		$Call = (
+			Invoke-WebRequest `
+				-Uri $Uri `
+				-Method DELETE `
+				-ContentType "application/json"
+		)
+
+	}
+
+	END {
+
+		# Check response status code #
+		if ($Call.StatusCode -eq '204') {
+			Write-Verbose "User Removed Successfully!"
+			Write-Output $Call.StatusCode
+		} else {
+			Write-Error "Failed to add user!"
+		}
+
+    }
+    
+}
