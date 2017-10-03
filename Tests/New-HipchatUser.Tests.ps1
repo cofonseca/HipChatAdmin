6a52796905f85d003967637da86b96a11ceeb7b9
mdavis332 <mdavis332@gmail.com>
2017-10-03 17:14:00 -0500

    Mock API call
    First pass at mocking Invoke-WebRequest so API calls are simulated. I've cobbled this together but haven't yet tested. If @cofonseca can test and confirm it's working, then it can be expanded to other Pester tests with Invoke-WebRequest commands.

--- Tests/New-HipchatUser.Tests.ps1
+++ Tests/New-HipchatUser.Tests.ps1
@@ -1,16 +1,36 @@
 ﻿Import-Module PHipChatAdmin
 
 Describe "New-HipchatUser" {
-
-    Context "When the API call is sent" {
-
-        It "Should return a 201 Status Code" {
-            New-HipchatUser -FirstName 'Pester' -LastName 'Test' -ApiToken $ApiToken | 
-            Should Be '201'
+    InModuleScope PHipChatAdmin {
+        
+        Context "When the API call is sent" {
+            
+            It 'Attempts to invoke web api' {
+                Mock Invoke-WebRequest -MockWith {
+                    $request = @{'id' = '123456';
+                                'Content' = 'someContent';
+                                'Status' = 'someStatus';
+                                'StatusCode' = '201'
+                                }
+                    return $request
+                }
+                $global:result = New-HipchatUser -FirstName 'Pester' -LastName 'Test' -ApiToken $ApiToken
+            }
+            
+            It 'Should be mocked' {
+                $MockParams = @{
+                    CommandName = 'Invoke-WebRequest'
+                    Times = 1
+                    Exactly = $true
+                }
+                Assert-MockCalled @MockParams
+            }
+            
+            It "Should return a 201 Status Code" {
+                $result.StatusCode.ToString() | Should Be '201'
+            }
         }
-    }
 
-    AfterAll {
-        Remove-HipchatUser -MentionName 'PesterTest'
     }
+    
 }
